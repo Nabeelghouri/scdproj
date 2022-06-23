42105e1c43e26e6a40aa29426963b5607bb89441
Nabeelghouri <99132789+Nabeelghouri@users.noreply.github.com>
2022-06-23 14:06:38 +0500

    Add files via upload

---
+++ ScdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.travelandtoursim.scdproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScdProjectApplication.class, args);
+	}
+
+}
